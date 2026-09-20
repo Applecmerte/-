ab8e3393d83a44fefd91aa2111d93881146cedca
Клим <Klim.hin.bo@gmail.com>
2026-09-20 17:32:37 +0300

    второе задание

---
+++ 1.3.2.3/1.3.2.3/1.3.2.3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double Q, T, p;
+	short N;
+
+	cout << "введите начальную стоимость оборудования, списание за 1-й год, процент и количество лет (Q, T, p, N): " << endl;
+	cin >> Q >> T >> p >> N;
+
+	if (N <= 0) {
+		cout << "неправильное выбран промежуток времени" << endl;
+		return 0;
+	}
+
+	for (; N != 0; --N) {
+		Q -= T;
+		cout << Q << endl;
+		T *= (1.0 - p / 100.0);
+	}
+
+	return 0;
+}
